300088ae2da1f74d933b66376c6b71c790271856
Kevin Eriksson <kevin_eriksson99@hotmail.se>
2025-01-07 18:13:49 +0100

    Added user login and post (frontend)

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import { Roboto } from 'next/font/google';
+import './globals.css';
+import Nav from './auth/Nav';
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700'], 
+  variable: '--font-roboto',
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`mx-4 md:mx-48 xl:mx-96 ${roboto.className} bg-gray-200`}>
+        <Nav />
+        {children}
+      </body>
+    </html>
+  );
+}
+
